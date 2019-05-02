3cbfe07dc4d46dc2541ea4a824b81044d62f3e9c
Jonathan Boos <gameguy37@gmail.com>
2019-05-02 14:55:49 -0400

    initial commit

---
+++ music_project/db/seeds.rb
@@ -0,0 +1,19 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+b1 = Band.create(name: 'Phish')
+b2 = Band.create(name: 'moe.')
+b3 = Band.create(name: 'Andy Frasco and the U.N.')
+b4 = Band.create(name: 'Fleetwood Mac')
+b5 = Band.create(name: 'Kikogaku Moyo')
+b6 = Band.create(name: 'Vulfpeck')
+b7 = Band.create(name: 'The Dismemberment Plan')
+b8 = Band.create(name: 'Radiohead')
+b9 = Band.create(name: 'Spafford')
+b10 = Band.create(name: 'Lawrence')
+
